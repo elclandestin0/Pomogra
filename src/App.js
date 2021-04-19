51d98b7e095be5177abffd515ee93c056e03c2c7
Memo Khoury <majd.khoury93@gmail.com>
2021-04-19 15:35:52 -0400

    feat: chains spawning rings

--- src/App.js
+++ src/App.js
@@ -1,50 +1,55 @@
 // react imports
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-// aframe imports 
+// aframe imports
 import "aframe";
-import {Entity, Scene} from 'aframe-react';
+import { Entity, Scene } from "aframe-react";
 
 // eth imports
 import web3 from "./ethereum/web3";
 import pomogra from "./ethereum/pomogra";
 
 const App = () => {
-  useEffect(()=>{
+  // chain state
+  const [chain, setChain] = useState([]);
+  useEffect(() => {
     const getContract = async () => {
-      const accounts = await web3.eth.getAccounts();
       const chain = await pomogra.methods.chain().call();
       console.log(chain);
-    }
+      setChain(chain);
+    };
     getContract();
-  },[])
+  }, []);
+  const returnChain = chain.map((paper, index) => {
+    
+    const message = paper.message;
+    const owner = paper.owner;
+    const paperType = paper.paperType;
+    
+    // if the index is even, rotate x by 90 degrees
+    let position = `${index / 5}, 1, 0`;
+    let rotation;
+    index % 2 == 0 ? rotation = "90 0 0" : rotation = "0 0 0";
+    return (
+      <Entity position={position} rotation={rotation} geometry="primitive: torus; radius: 0.125; radiusTubular: 0.010">
+        <Entity position="0 .225 0" text={{ value: message}} geometry="primitive: plane; width: 1.030; height: 0.100;"/>
+      </Entity>
+    );
+  });
   return (
     <Scene>
+      <a-camera>
+        <a-cursor material="color: #ffffff"></a-cursor>
+      </a-camera>
       <Entity
-        geometry="primitive: box"
-        position="-1 0.5 -3"
-        rotation="0 45 0"
-        material="color: #4CC3D9"
-      />
-      <Entity
-        geometry="primitive: sphere; radius: 1.25;"
-        position="0 1.25 -5"
-        material="color: #EF2D5E"
-      />
-      <Entity
-        geometry="primitive: cylinder; radius: 0.5, height: 1.5"
-        position="1 0.75 -3"
-        material="color: #FFC65D"
-      />
-      <Entity
-        geometry="primitive: plane; width: 4; height: 4"
+        geometry="primitive: plane; width: 10; height: 10"
         position="0 0 -4"
         rotation="-90 0 0"
         material="color: #7BC8A4"
       />
-      <Entity text={{value: 'Hello, Yo!'}}/>
+      {returnChain}
     </Scene>
   );
-}
+};
 
 export default App;
