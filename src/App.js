8e6afb52bae6060616c88ffe0562588f28893b9d
mkhoury93 <majd.khoury93@gmail.com>
2021-04-21 17:40:52 -0400

    feat: message shows on hover

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import React, { useEffect, useState } from "react";
 // aframe imports
 import "aframe";
 import { Entity, Scene } from "aframe-react";
-
 // eth imports
 // import web3 from "./ethereum/web3";
 import pomogra from "./ethereum/pomogra";
 
+// pomogra aframe components
+require("./components/aframe/show-message");
+
 const App = () => {
   // chain state
   const [chain, setChain] = useState([]);
+
+  // show message state
+  const [show, setShow] = useState(false);
   useEffect(() => {
     const getContract = async () => {
       const chain = await pomogra.methods.chain().call();
@@ -20,9 +25,13 @@ const App = () => {
     getContract();
   }, []);
 
-  const handleClick = () => {
-    console.log("clicked");
-  };
+  function showMessage() {
+    setShow(true);
+  }
+
+  function removeMessage() {
+    setShow(false);
+  }
 
   const returnChain = chain.map((paper, index) => {
     const message = paper.message;
@@ -30,36 +39,42 @@ const App = () => {
     const paperType = paper.paperType;
 
     // if the index is even, rotate x by 90 degrees
+
     let position = `${index / 5}, 1, 0`;
     let rotation;
-    index % 2 === 0 ? (rotation = "-90 0 0") : (rotation = "0 0 0");
+    index % 2 === 0 ? (rotation = "0 0 0") : (rotation = "-90 0 0");
+
     const animationOptions = `startEvents: mouseleave; property: rotation; dur: 1000; from: 0 0 0; to: ${rotation}; dir: normal; easing: linear; loop: false;`;
+    const showMessageOptions = `show: ${show}`;
     return (
-      <a-entity
+      <Entity
+        class="clickable"
         key={index}
         position={position}
         rotation={rotation}
         geometry="primitive: torus; radius: 0.125; radiusTubular: 0.010"
         animation="startEvents: mouseenter; property: rotation; dur: 1000; from: 0 0 0; to: 0 360 0; dir: normal; easing: linear; loop: true;"
         animation__back={animationOptions}
+        events={{
+          mouseenter: showMessage.bind(this),
+          mouseleave: removeMessage.bind(this),
+        }}
+        // show-message={showMessageOptions}
       >
-        {/* <Entity
-          position="0 .225 0"
-          text={{ value: message }}
-          geometry="primitive: plane; width: 1.030; height: 0.100;"
-        /> */}
-      </a-entity>
+        {show && (
+          <Entity
+            position="0 .225 0"
+            text={{ value: message }}
+            geometry="primitive: plane; width: 1.030; height: 0.100;"
+          />
+        )}
+      </Entity>
     );
   });
   return (
     <Scene>
       <a-camera>
         <a-cursor
-          events={{
-            mouseenter: () => {
-              handleClick();
-            },
-          }}
           animation__click="property: scale; startEvents: click; easing: easeInCubic; dur: 150; from: 0.2 0.2 0.2; to: 1 1 1"
           // animation__fusing="property: scale; startEvents: fusing; easing: easeInCubic; dur: 1500; from: 1 1 1; to: 0.2 0.2 0.2"
           // animation__mouseleave="property: scale; startEvents: mouseleave; easing: easeInCubic; dur: 500; to: 1 1 1"
---
+++ src/components/aframe/show-message.js
@@ -0,0 +1,32 @@
+const AFRAME = window.AFRAME;
+
+AFRAME.registerComponent("log-console", {
+  schema: {
+    message: { type: "string", default: "" },
+  },
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    el.addEventListener("mouseenter", () => {
+      console.log("hello" + data.message);
+    });
+  },
+});
+
+AFRAME.registerComponent("show-message", {
+  schema: {
+    show: { type: "boolean", default: "false" },
+  },
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+    el.addEventListener("mouseenter", () => {
+      data.show = true;
+      console.log(data.show);
+    });
+    el.addEventListener("mouseleave", () => {
+      data.show = false;
+      console.log(data.show);
+    });
+  },
+});
