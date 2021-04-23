d6edb67cb2c7ede46ae974b6539a0daac4b35928
mkhoury93 <majd.khoury93@gmail.com>
2021-04-23 09:52:38 -0400

    feat: added aframe-gui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 
 // aframe imports
 import "aframe";
-import "aframe-gui";
 import { Entity, Scene } from "aframe-react";
 
 // eth imports
@@ -13,6 +12,7 @@ import pomogra from "./ethereum/pomogra";
 // pomogra aframe components
 import Ring from "./components/aframe/Ring";
 require("./components/aframe/show-message");
+require("aframe-ui-widgets");
 
 const App = () => {
   // chain state
@@ -42,31 +42,25 @@ const App = () => {
     );
   });
   return (
-    <Scene>
-      <a-gui-input
-        width="2.5"
-        height="0.75"
-        // font-family="assets/fonts/PermanentMarker-Regular.ttf"
-        font-size="0.2"
-        value="123"
-        margin="0 0 0.05 0"
-        position="0 1 0"
-      ></a-gui-input>
-      <a-camera>
-        <a-cursor
-          animation__click="property: scale; startEvents: click; easing: easeInCubic; dur: 150; from: 0.2 0.2 0.2; to: 1 1 1"
-          cursor="fuse: true; fuseTimeout: 1"
-          material="color: #ffffff"
-        ></a-cursor>
-      </a-camera>
-      <Entity
-        geometry="primitive: plane; width: 10; height: 10"
-        position="0 0 -4"
-        rotation="-90 0 0"
-        material="color: #7BC8A4"
-      />
-      {returnChain}
-    </Scene>
+    <div>
+      <Scene>
+        <a-entity id="buttonStd" ui-button position="0 1 0" rotation="45 0 0"></a-entity>
+        <a-camera>
+          <a-cursor
+            animation__click="property: scale; startEvents: click; easing: easeInCubic; dur: 150; from: 0.2 0.2 0.2; to: 1 1 1"
+            cursor="fuse: true; fuseTimeout: 1"
+            material="color: #ffffff"
+          ></a-cursor>
+        </a-camera>
+        <Entity
+          geometry="primitive: plane; width: 10; height: 10"
+          position="0 0 -4"
+          rotation="-90 0 0"
+          material="color: #7BC8A4"
+        />
+        {returnChain}
+      </Scene>
+    </div>
   );
 };
 
